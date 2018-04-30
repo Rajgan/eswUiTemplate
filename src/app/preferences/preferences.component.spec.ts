facca702bfa020d80e82c45f05d71240560e512e
Martin Higgins <mhiggins@eshopworld.com>
2018-04-30 09:33:15 +0100

    update unit tests

--- src/app/preferences/preferences.component.spec.ts
+++ src/app/preferences/preferences.component.spec.ts
@@ -1,17 +1,19 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 import { PreferencesComponent } from './preferences.component';
 import { RouterModule, Routes } from '@angular/router';
-import { TranslateModule } from '@ngx-translate/core';
+import { TranslateModule, TranslateService } from '@ngx-translate/core';
 
 describe('Component: PreferencesComponent', () => {
   let component: PreferencesComponent;
   let fixture: ComponentFixture<PreferencesComponent>;
+  let translateService: TranslateService;
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
       imports: [
         TranslateModule.forRoot()
       ],
+      providers: [ TranslateService ],
       declarations: [ PreferencesComponent ]
     })
     .compileComponents();
@@ -20,11 +22,33 @@ describe('Component: PreferencesComponent', () => {
   beforeEach(() => {
     fixture = TestBed.createComponent(PreferencesComponent);
     component = fixture.componentInstance;
+    translateService = fixture.debugElement.injector.get(TranslateService);
     fixture.detectChanges();
   });
 
   it('should create', () => {
+    localStorage.setItem('lang', 'en');
     expect(component).toBeTruthy();
+  });
+
+  it('should update the translateService with the language', () => {
+    const spy = spyOn(translateService, 'use').and.callThrough();
+    const newLang = 'de';
+    component.languageChanged(newLang);
+    expect(spy).toHaveBeenCalledWith(newLang);
+    expect(localStorage.getItem('lang')).toEqual(newLang);
+  });
+
+  it('should return the current language', () => {
+    const lang = 'ie';
+    translateService.currentLang = lang;
+    expect(component.currentLang).toEqual(lang);
+  });
+
+  it('will return a translated language file', () => {
+    const spy = spyOn(translateService, 'instant');
+    const translatedLang = component.currentLangTranslated;
+    expect(spy).toHaveBeenCalledTimes(1);
   });
 
 });
